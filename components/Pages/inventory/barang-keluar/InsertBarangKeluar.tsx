6e39791ddf7f11e3f808260adcd3dc94aa244304
Ricko Caesar Aprilla Tiaka <78606675+younixue3@users.noreply.github.com>
2025-01-06 16:15:50 +0800

    fix

--- components/Pages/inventory/barang-keluar/InsertBarangKeluar.tsx
+++ components/Pages/inventory/barang-keluar/InsertBarangKeluar.tsx
@@ -38,9 +38,9 @@ export function InsertBarangKeluar({ GetDetailBarang, queryClient }) {
 
   const categoryCakes = useGetCategoryCakes({ isSelect: true });
 
-  useEffect(() => {
-    setValue('category_cake_id', initialValue?.category_cake[0]?.id);
-  }, [setValue, initialValue]);
+  // useEffect(() => {
+  //   setValue('category_cake_id', initialValue?.category_cake[0]?.id);
+  // }, [setValue, initialValue]);
 
   const onSubmit = form => {
     const payload = {
