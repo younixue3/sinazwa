ad84e3180fd844ed0ccd4db0dc4f7e407e0cad1e
younixue3 <bussiness.needs.rctka@gmail.com>
2025-03-09 11:26:26 +0800

    fix(barang-keluar): add back category

--- components/Pages/inventory/barang-keluar/InsertBarangKeluar.tsx
+++ components/Pages/inventory/barang-keluar/InsertBarangKeluar.tsx
@@ -6,26 +6,38 @@ import Swal from 'sweetalert2';
 import InputComponent from 'components/Form/InputComponent';
 import ButtonComponent from 'components/Button/ButtonComponent';
 import useBarangKeluar from 'utils/api/inventaris/use-barang-keluar';
+import useGetCategoryCakes from 'utils/api/category_cake/use-get-category-cakes';
+import { useRef } from 'react';
 
 export function InsertBarangKeluar({ GetDetailBarang, queryClient }) {
   const initialValue = GetDetailBarang?.data?.data;
+  const categoryCakesRef = useRef();
   const BarangKeluar = useBarangKeluar();
 
   const schema = yup.object({
     qty_item: yup.number().required('Jumlah Barang harus di isi.'),
+    category_cake_id: yup.object().required('Category Cake'),
     description: yup.string().required('Deskripsi harus di isi.')
   });
   const {
     register,
     handleSubmit,
     reset,
+    control,
+    setValue,
+    clearErrors,
     formState: { errors }
   } = useForm({ values: initialValue, resolver: yupResolver(schema) });
+
+
+  const categoryCakes = useGetCategoryCakes({ isSelect: true });
+
 
   const onSubmit = form => {
     const payload = {
       ...form,
       inventory_id: initialValue.id,
+      category_cake_id: form.category_cake_id.value,
       tipe_riwayat: 2
     };
 
@@ -77,6 +89,32 @@ export function InsertBarangKeluar({ GetDetailBarang, queryClient }) {
           error={errors.description?.message}
           register={register('description')}
         />
+        <div className={'flex w-full flex-col'}>
+          <label className={'text-sm'}>Kategori Kue</label>
+          <Controller
+            control={control}
+            name="category_cake_id"
+            render={({ field }) => (
+              <Select
+                className={'text-black'}
+                {...field}
+                instanceId="category_cake_id"
+                options={categoryCakes.data || []}
+                onChange={input => {
+                  setValue('category_cake_id', input);
+                  clearErrors('category_cake_id');
+                }}
+                ref={categoryCakesRef}
+                isDisabled={categoryCakes.isLoading}
+              />
+            )}
+          />
+          {errors.category_cake_id && (
+            <span className="text-xs text-red-500">
+              {errors.category_cake_id.message.toString()}
+            </span>
+          )}
+        </div>
         <ButtonComponent
           type={'submit'}
           text={'Submit'}
