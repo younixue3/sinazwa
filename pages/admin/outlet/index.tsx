b88baf9834dac13cd21443bcc3ed869347ab2ac7
younixue3 <bussiness.needs.rctka@gmail.com>
2025-06-23 08:54:49 +0800

    feat(outlet): add delivery threshold component to admin outlet page
    Integrate OutletThreshold component to display delivery threshold data fetched from API

--- pages/admin/outlet/index.tsx
+++ pages/admin/outlet/index.tsx
@@ -3,13 +3,25 @@ import CardComponent from 'components/Card/CardComponent';
 import useGetOutlet from 'utils/api/outlet/use-get-outlet';
 import { CreateOutlet } from 'components/Pages/outlet/CreateOutlet';
 import { OutletTableComponent } from 'components/Table/OutletTableComponent';
+import OutletThreshold from 'components/Pages/admin/outlet/OutletThreshold';
+import useGetDeliveryThreshold from 'utils/api/DeliveryThreshold/use-get-deliveries-threshold';
 
 export default function Outlet() {
   const GetOutlet = useGetOutlet({ isSelect: false });
 
+  const { data: deliveriesThresholdData, isLoading: isLoadingThreshold } =
+    useGetDeliveryThreshold();
+
+  console.log(deliveriesThresholdData);
+
   return (
     <AdminLayout>
       <main className={'p-4 grid grid-cols-6 gap-3'}>
+        <div className={'col-span-6'}>
+          {!isLoadingThreshold && (
+            <OutletThreshold data={deliveriesThresholdData} />
+          )}
+        </div>
         <div className={'col-span-6'}>
           <CardComponent>
             <div className={'p-5'}>
